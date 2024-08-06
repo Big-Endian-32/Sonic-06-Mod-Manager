1d48773bf1d4d5305eef62e436afec3781d78eae
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-08-06 04:38:37 +0100

    Lua.Wrappers: added MessageTable wrapper

---
+++ SonicNextModManager/Lua/Callback/TextFunctions.cs
@@ -0,0 +1,20 @@
+﻿using SonicNextModManager.Helpers;
+using SonicNextModManager.Lua.Attributes;
+using SonicNextModManager.Lua.Wrappers.Text;
+
+namespace SonicNextModManager.Lua.Callback
+{
+    public class TextFunctions
+    {
+        [LuaCallback]
+        public static DynValue LoadMessageTable(string in_path)
+        {
+            var file = ArchiveHelper.GetArchiveFile(in_path);
+
+            if (file == null)
+                return DynValue.Nil;
+
+            return UserData.Create(new MessageTableWrapper(file));
+        }
+    }
+}
---
+++ SonicNextModManager/Lua/Wrappers/Text/MessageTableWrapper.cs
@@ -0,0 +1,44 @@
+﻿using Marathon.Formats.Archive;
+using Marathon.Formats.Text;
+using SonicNextModManager.Helpers;
+using SonicNextModManager.Lua.Attributes;
+using SonicNextModManager.Lua.Interfaces;
+
+namespace SonicNextModManager.Lua.Wrappers.Text
+{
+    [LuaUserData]
+    public class MessageTableWrapper : MarathonWrapper, ILuaUserDataDescriptor
+    {
+        private MessageTable _messageTable;
+
+        public MessageTableWrapper() { }
+
+        public MessageTableWrapper(U8ArchiveFile in_file) : base(in_file)
+        {
+            _messageTable = IOHelper.LoadMarathonTypeFromBuffer<MessageTable>(File.Data);
+        }
+
+        public void Register()
+        {
+            UserData.RegisterType<Message>();
+        }
+
+        public Message GetMessage(string in_name)
+        {
+            return _messageTable.Data.Messages.Where(x => x.Name == in_name).FirstOrDefault()!;
+        }
+
+        public void SetMessage(string in_name, string in_text, params string[] in_placeholders)
+        {
+            var msg = GetMessage(in_name);
+
+            msg.Text = in_text;
+            msg.Placeholders = in_placeholders;
+        }
+
+        public void Save()
+        {
+            Save(_messageTable);
+        }
+    }
+}
