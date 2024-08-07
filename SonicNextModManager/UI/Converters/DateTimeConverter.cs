d8129fc438ba898e797b16ca8e912e5e2b81ecc0
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-08-07 05:50:40 +0100

    EditorWindow: use localised date format

---
+++ SonicNextModManager/UI/Converters/DateTimeConverter.cs
@@ -0,0 +1,25 @@
+﻿namespace SonicNextModManager.UI.Converters
+{
+    [ValueConversion(typeof(DateTime), typeof(string))]
+    public class DateTimeConverter : IValueConverter
+    {
+        public object Convert(object in_value, Type in_targetType, object in_param, CultureInfo in_culture)
+        {
+            if (in_value is string out_str)
+            {
+                if (DateTime.TryParse(out_str, out var out_result))
+                    return out_result;
+            }
+
+            return in_value;
+        }
+
+        public object ConvertBack(object in_value, Type in_targetType, object in_param, CultureInfo in_culture)
+        {
+            if (in_value is DateTime out_dt)
+                return out_dt.ToString(LocaleService.Localise("Common_DateFormat"));
+
+            return in_value;
+        }
+    }
+}
