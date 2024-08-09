3643f35e3a32533befe3b13de82dc65271851356
Knux <knuxfan@live.co.uk>
2024-08-09 02:42:34 +0100

    Sound Bank Wrapper
    Shamelessly cloned from the Message Table Wrapper, also needs an edit making to Marathon for neatness, but that can be done later; this at least functions.

---
+++ SonicNextModManager/Lua/Callback/AudioFunctions.cs
@@ -0,0 +1,15 @@
+﻿using SonicNextModManager.Lua.Attributes;
+using SonicNextModManager.Lua.Wrappers;
+using SonicNextModManager.Lua.Wrappers.Audio;
+
+namespace SonicNextModManager.Lua.Callback
+{
+    public class AudioFunctions
+    {
+        [LuaCallback]
+        public static DynValue LoadSoundBank(string in_path)
+        {
+            return MarathonWrapper.RegisterWrapperToArchiveFile<SoundBankWrapper>(in_path);
+        }
+    }
+}
---
+++ SonicNextModManager/Lua/Wrappers/Audio/SoundBankWrapper.cs
@@ -0,0 +1,68 @@
+﻿using Marathon.Formats.Archive;
+using Marathon.Formats.Audio;
+using SonicNextModManager.Helpers;
+using SonicNextModManager.Lua.Attributes;
+using SonicNextModManager.Lua.Interfaces;
+
+namespace SonicNextModManager.Lua.Wrappers.Audio
+{
+    [LuaUserData]
+    public class SoundBankWrapper : MarathonWrapper, ILuaUserDataDescriptor
+    {
+        private SoundBank _soundBank;
+
+        public SoundBankWrapper() { }
+
+        public SoundBankWrapper(U8ArchiveFile in_file) : base(in_file)
+        {
+            _soundBank = IOHelper.LoadMarathonTypeFromBuffer<SoundBank>(File.Data);
+        }
+
+        public void Register()
+        {
+            UserData.RegisterType<Cue>();
+        }
+
+        public Cue GetCue(string in_name)
+        {
+            return _soundBank.Data.Cues.Where(x => x.Name == in_name).FirstOrDefault()!;
+        }
+
+        public Cue[] GetCues()
+        {
+            return [.. _soundBank.Data.Cues];
+        }
+
+        public void SetCue(string in_name, uint in_category, float in_unknownSingle, float in_radius, string in_stream)
+        {
+            if (_soundBank.Data.Cues.Any(x => x.Name == in_name))
+            {
+                var cue = GetCue(in_name);
+
+                cue.Category = in_category;
+                cue.UnknownSingle = in_unknownSingle;
+                cue.Radius = in_radius;
+                cue.Stream = in_stream;
+            }
+            else
+            {
+                // TODO: Give Marathon Cue Constructors and crush this down like in the Message Table Wrapper.
+                var cue = new Cue()
+                {
+                    Name = in_name,
+                    Category = in_category,
+                    UnknownSingle = in_unknownSingle,
+                    Radius = in_radius,
+                    Stream = in_stream
+                };
+
+                _soundBank.Data.Cues.Add(cue);
+            }
+        }
+
+        public void Save()
+        {
+            Save(_soundBank);
+        }
+    }
+}
