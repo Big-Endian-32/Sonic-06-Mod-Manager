8a319d0dc231ef7c768910aa951f9220711e2eed
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-08-21 22:45:35 +0100

    SonicNextModLoader (WIP)

---
+++ SonicNextModLoader/stdafx.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <xtl.h>
+
+#include <cassert>
+#include <cstdint>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include <Helpers.h>
+#include <INIReader.h>
+#include <StringHelper.h>
+
+#include "HookService.h"
